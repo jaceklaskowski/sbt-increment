9f7548bbfc0e2fe6b7c0ba17de3f20e1dfdcce45
Jacek Laskowski <jacek@japila.pl>
2014-05-16 01:50:27 +0200

    New (fake) two tasks

--- src/main/scala/sbtincrement/Plugin.scala
+++ src/main/scala/sbtincrement/Plugin.scala
@@ -4,10 +4,14 @@ import sbt._
 import Keys._
 
 object Plugin extends sbt.Plugin {
-  lazy val incrementVersionPatch = taskKey[String]("Increments version at patch position")
+  lazy val incrementMajor = taskKey[String]("Increments version at major position")
+  lazy val incrementMinor = taskKey[String]("Increments version at minor position")
+  lazy val incrementPatch = taskKey[String]("Increments version at patch position")
 
   lazy val baseIncrementSettings: Seq[Def.Setting[_]] = Seq(
-    incrementVersionPatch := "1.2.3"
+    incrementMajor := "1.0.0",
+    incrementMinor := "0.1.0",
+    incrementPatch := "0.0.1"
   )
 
   lazy val incrementSettings: Seq[Def.Setting[_]] = baseIncrementSettings
